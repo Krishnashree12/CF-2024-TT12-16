7b3ee4ce39173c2a19d0fdac5f93ed8ae5fce195
dharsh1727 <dharsh1727s@gmail.com>
2025-08-10 10:36:43 +0530

    Update fifo_memory.v

--- fifo_memory.v
+++ fifo_memory.v
@@ -1,5 +1,5 @@
 module fifo_memory #(parameter DATA_WIDTH = 8, ADDR_WIDTH = 4)(
-    input wclk,rclk,
+    input wclk, rclk,
     input [ADDR_WIDTH-1:0] waddr, raddr,
     input [DATA_WIDTH-1:0] wdata,
     input wen, ren,
@@ -8,14 +8,15 @@ module fifo_memory #(parameter DATA_WIDTH = 8, ADDR_WIDTH = 4)(
     reg [DATA_WIDTH-1:0] mem [(2**ADDR_WIDTH)-1:0];
 
     always @(posedge wclk) begin
-        if (wen && !full) begin
-          mem[waddr] <= wdata;
+        if (wen) begin
+            mem[waddr] <= wdata;
+        end
     end
-        
+
     always @(posedge rclk) begin
-        if (ren && !empty) 
+        if (ren) begin
           rdata <= mem[raddr];
-        else 
-          rdata <= {WIDTH{1'b0}};
+       end
     end
- endmodule
+
+endmodule
